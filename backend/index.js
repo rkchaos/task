692e4421a47cabd7afac3feb092a28ba293168f8
Raj kumar <rk0346101@gmail.com>
2025-01-07 02:10:13 +0530

    comiits

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,9 @@ const task=require("./router/task")
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: 'http://localhost:5173', 
-    credentials: true, 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Authorization', 'Content-Type'], 
-    optionsSuccessStatus: 200, 
+    origin: true,
+    credentials: true,
+    optionsSuccessStatus: 200,
 }));
 
 
