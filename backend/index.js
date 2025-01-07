fa1abbfea5de5010d6e8c45f53a5e439075cdb71
Raj Kumar <101791989+rkchaos@users.noreply.github.com>
2025-01-07 12:36:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const task=require("./router/task")
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: 'https://taskmanager-one-mu.vercel.app', // Frontend URL
+    origin: 'https://task-front-mu.vercel.app', // Frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
     credentials: true, // Allow cookies and credentials
 }))
